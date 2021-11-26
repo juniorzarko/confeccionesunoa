31b5645f60825b1ca4949565ab85f594f8131689
Hector Holguin <kikitanflo@gmail.com>
2021-11-26 10:11:26 -0500

    merge de archivos 3

--- front/mod_operacion.php
+++ front/mod_operacion.php
@@ -1,48 +1,58 @@
 <div class="container flex p-2 shadow bg-transparent rounded">
-      <form class=" gx-3 gy-2 ">
+      <form class=" gx-1 gy-2 ">
         <div class="col-md-12"> <h2 class="text-center">Creación de Operaciones</h2></div> 
         <div class="row"> 
-             <div class="col-md-5 ">
-                <div class="input-group">
-                 <div class="input-group-text">Fecha:
+            <div class="col-md-4 ">
+              <div class="input-group">
+                <div class="input-group-text">Fecha:
                  </div>
                   <input type="date" class="form-control" id="fecha" placeholder="">
-                </div>
               </div>
-                 <div class="col-md-5 ">
+            </div>
+            <div class="col-md-4 ">
                 <div class="input-group">
                   <div class="input-group-text">Nombre</div>
                      <input type="text" class="form-control" id="nombre_lote" placeholder="Nombre">
                 </div>
-            </div> 
+            </div>
+            <div class="col-md-4">
+            <div class="input-group">
+              <div class="input-group-text">Costo:
+              </div>
+                  <input type="number" class="form-control" id="" placeholder="Costo">
+            </div>
+          </div>          
         </div> 
-        <div class="row">   
-        <div class="col-md-5 mt-2">
+        <!-- <div class="row">   
+          <div class="col-md-5 mt-2">
             <div class="input-group">
               <div class="input-group-text">Costo:
               </div>
                   <input type="number" class="form-control" id="" placeholder="Costo">
             </div>
           </div>         
-        <div class="col-md-5 mt-2">
+          <div class="col-md-5 mt-2">
               <div class="input-group">
                   <div class="input-group-text">
                    Unidades
                   </div>
-                  <input type="number" class="form-control" id="cantidad" placeholder="Cantidad">
+                  <input type="number" class="form-control" id="" placeholder="Unidades">
               </div>
-          </div>          
-         
-        </div>    
-         
+          </div>         
+        </div>         -->
       </form>
-
       <nav class="navbar navbar-light bg-light">
-       <form class="container-fluid justify-content-center">
+        <form class="container-fluid justify-content-center">
          
           <button class="btn btn-sm btn-outline-primary" type="button">Editar</button>
           <button class="btn btn-sm btn-outline-primary mx-3" type="button">Eliminar</button>
           <button class="btn btn-sm btn-outline-primary" type="button">Guardar</button>
-       </form>
-    </nav>
+        </form>
+      </nav>
 </div>
+  
+
+  
+
+ 
+
